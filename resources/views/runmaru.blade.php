90ef551769f15b0654b68648f0f579f9ba4378f2
eto0831 <eto0831@gmail.com>
2026-02-18 22:56:44 +0900

    initial Sail setup + DB + sessions fix + RunMaru top

---
+++ resources/views/runmaru.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Run丸</title>
+</head>
+
+<body>
+    <h1>Run丸 🏃‍♂️⭕</h1>
+    <p>今日から走った日を記録する。</p>
+</body>
+
+</html>
